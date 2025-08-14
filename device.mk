0996372466835453ac76eed0de081bb9aec43cc3
XingKongTanSuo <3329989998@qq.com>
2025-08-14 12:57:43 +0800

    Add files via upload

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/zyb/k69v164k419
+LOCAL_PATH := device/zyb/k69v1_64_k419
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
