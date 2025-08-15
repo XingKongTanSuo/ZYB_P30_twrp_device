14ff9af6793c2774675e00821a74722267e77af4
Momo5418 <momo5418@mocn.top>
2025-08-15 10:33:03 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,14 +21,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES += \
-    bootctrl
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
